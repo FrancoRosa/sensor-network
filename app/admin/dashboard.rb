6c60728838d892ab835df29f06e4a5834fee6916
FrancoRosa <km115.franco@gmail.com>
2020-06-14 12:23:34 -0500

    Change columns on Admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,25 +2,37 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
+    # div class: "blank_slate_container", id: "dashboard_default_message" do
+    #   span class: "blank_slate" do
+    #     span I18n.t("active_admin.dashboard_welcome.welcome")
+    #     small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    #   end
+    # end
 
     # Here is an example of a simple dashboard with columns and panels.
     #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    columns do
+      column do
+        panel "Recent Sensor Updates" do
+          ul do
+            SensorHistory.order('created_at DESC').limit(10).map do |sensor|
+              parent = sensor.sensor
+              li link_to("#{parent.features}: #{sensor.value} #{parent.variable.unit} #{sensor.created_at}", admin_sensor_path(parent))
+            end
+          end
+        end
+      end
+      column do  
+      panel "Recent Actuator Updates" do
+          ul do
+            ActuatorHistory.order('created_at DESC').limit(10).map do |actuator|
+              parent = actuator.actuator
+              li link_to("#{parent.features}: #{actuator.expected_status} #{actuator.created_at}", admin_actuator_path(parent))
+            end
+          end
+        end
+      end
+    end
 
     #   column do
     #     panel "Info" do
