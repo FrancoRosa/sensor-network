f7411d07826ec3afcecdc3d0036e2d695eb64d04
FrancoRosa <km115.franco@gmail.com>
2020-08-07 22:22:26 -0500

    Fix webhook response

--- api-client/api-webhook.py
+++ api-client/api-webhook.py
@@ -4,7 +4,7 @@ import requests
 url = 'http://localhost:3000/api/sensors'
 # url = 'https://sensor-network-lora.herokuapp.com/api/sensors'
 
-data = {"hub.mode": "subscribe", "hub.challenge": "1817199792", "hub.verify_token": "LapupaExtrema", "id": "api"} 
+data = {"hub.mode": "subscribe", "hub.challenge": "1817199792", "hub.verify_token": "secrettoken", "id": "api"} 
 
 response = requests.get(url, json=data)
 print(">>> tx:", data)
